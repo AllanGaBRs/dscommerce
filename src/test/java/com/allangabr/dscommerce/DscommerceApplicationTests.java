7a4a334192c057f360c1b60dcf0a7dea3de875b2
allangabr <allanmediogames@gmail.com>
2025-05-25 16:11:28 -0300

    created project

---
+++ src/test/java/com/allangabr/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allangabr.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

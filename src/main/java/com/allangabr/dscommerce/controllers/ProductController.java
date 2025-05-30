1b81a95f52013b620b900ee06601a77960e0ba78
allangabr <allanmediogames@gmail.com>
2025-05-29 22:46:19 -0300

    feat: test with repository

--- src/main/java/com/allangabr/dscommerce/controllers/ProductController.java
+++ src/main/java/com/allangabr/dscommerce/controllers/ProductController.java
@@ -1,16 +1,27 @@
 package com.allangabr.dscommerce.controllers;
 
+import com.allangabr.dscommerce.entities.Product;
+import com.allangabr.dscommerce.repositories.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping
     public String test(){
-        return "Ola mundo!";
+        Optional<Product> productOptional = productRepository.findById(1L);
+        Product product = productOptional.get();
+        return product.getName();
     }
 
 }
